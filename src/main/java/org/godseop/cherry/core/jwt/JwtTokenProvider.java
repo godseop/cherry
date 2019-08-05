58be4cf77a33863c0ebc628794e24767dfa56505
godseop <godseop@gmail.com>
2019-08-06 00:00:40 +0900

    jwt 업그레이드

--- src/main/java/org/godseop/cherry/core/jwt/JwtTokenProvider.java
+++ src/main/java/org/godseop/cherry/core/jwt/JwtTokenProvider.java
@@ -2,8 +2,11 @@ package org.godseop.cherry.core.jwt;
 
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.impl.crypto.MacProvider;
 import lombok.RequiredArgsConstructor;
+import org.godseop.cherry.core.exception.CherryException;
 import org.godseop.cherry.core.exception.InvalidJwtAuthenticationException;
+import org.godseop.cherry.core.model.Error;
 import org.godseop.cherry.dto.User;
 import org.godseop.cherry.service.UserService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,9 +14,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 @Component
 @RequiredArgsConstructor
@@ -32,18 +38,23 @@ public class JwtTokenProvider {
 
     public String createToken(String userId, String authCode) {
 
+        SecretKey signingKey = MacProvider.generateKey();
+
         Claims claims = Jwts.claims().setSubject(userId);
         claims.put("roles", authCode);
+        //claims.put("company", companyCode);
 
-        Date now = new Date();
+        Calendar calendar = new GregorianCalendar();
+        Date now = calendar.getTime();
         Date validity = new Date(now.getTime() + jwtProperties.getValidityInMs());
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
+        JwtBuilder builder = Jwts.builder()
+                .setClaims(claims)          // 인증정보
+                .setIssuedAt(now)           // 이슈
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
+                .signWith(SignatureAlgorithm.ES256, secretKey);
+
+        return builder.compact();
     }
 
     public Authentication getAuthentication(String token) {
@@ -65,11 +76,18 @@ public class JwtTokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody();
 
             return !claims.getBody().getExpiration().before(new Date());
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new InvalidJwtAuthenticationException("JWT 토큰이 만료되었거나 잘못되었습니다.");
+        } catch (ExpiredJwtException exception) {
+            throw new CherryException(Error.TOKEN_EXPIRED);
+        } catch (SignatureException exception) {
+            throw new CherryException(Error.TOKEN_INVALID);
+        } catch (MalformedJwtException exception) {
+            throw new CherryException(Error.TOKEN_INVALID);
         }
     }
 }
--- src/main/java/org/godseop/cherry/core/model/Error.java
+++ src/main/java/org/godseop/cherry/core/model/Error.java
@@ -9,18 +9,36 @@ import lombok.ToString;
 @ToString
 public enum Error {
 
+    /**
+     * SUCCESS
+     */
     OK("0000", "정상"),
 
-    USER_NOT_FOUND("1000", "등록되지 않은 사용자입니다."),
-    LOGIN_FAIL("1001", "사용자 인증에 실패했습니다."),
-    DUPLICATED_USER_ID("1002", "사용자 ID 중복입니다."),
+    /**
+     * PARAMETER ERROR
+     */
+    MANDATORY_VIOLATE("1000", "필수 파라미터 오류입니다."),
+    WRONG_PAGE_PARAMETER("1100", "페이지 파라미터 오류입니다"),
 
-    JWT_INVALID_OR_EXPIRED_TOKEN("8000", "JWT 토큰이 만료되었거나 잘못되었습니다."),
+    USER_NOT_FOUND("2000", "등록되지 않은 사용자입니다."),
+    LOGIN_FAIL("2100", "사용자 인증에 실패했습니다."),
+    DUPLICATED_USER_ID("2200", "사용자 ID 중복입니다."),
 
-    SERVICE_UNAVAILABLE("9998", "일시적인 서비스장애입니다. 관리자에게 문의해주세요."),
-    INTERNAL_SERVER_ERROR("9999", "서버 에러입니다. 관리자에게 문의해주세요."),
+
+    /**
+     * JWT TOKEN ERROR
+     */
+    TOKEN_INVALID("8000", "인증토큰 오류입니다."),
+    TOKEN_EXPIRED("8100", "인증토큰이 만료되었습니다."),
+    AUTH_NOT_GRANTED("8200", "해당 작업의 권한이 없습니다."),
 
 
+    /**
+     * SYSTEM ERROR
+     */
+    DATABASE_ERROR("9700", "데이터베이스 오류입니다."),
+    SERVICE_UNAVAILABLE("9800", "일시적인 서비스장애입니다. 관리자에게 문의해주세요."),
+    INTERNAL_SERVER_ERROR("9900", "치명적인 오류가 발생했습니다. 관리자에게 문의해주세요."),
     ;
 
     private final String code;
